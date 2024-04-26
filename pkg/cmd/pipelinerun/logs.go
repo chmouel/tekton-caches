a079d76e3ccb2125c3fd3c82c3d8ca56d194469a
Chmouel Boudjnah <chmouel@redhat.com>
2024-04-26 14:09:16 +0200

    Add -E flag to exit with the pr sate on unix shell
    Add the -E or --exit-with-pipelinerun-error flag to tkn pr logs to exit
    with the pipelinerun error after showing the logs.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/cmd/pipelinerun/logs.go
+++ pkg/cmd/pipelinerun/logs.go
@@ -15,6 +15,7 @@
 package pipelinerun
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strings"
@@ -25,6 +26,8 @@ import (
 	"github.com/tektoncd/cli/pkg/log"
 	"github.com/tektoncd/cli/pkg/options"
 	pipelinerunpkg "github.com/tektoncd/cli/pkg/pipelinerun"
+	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -82,6 +85,7 @@ Show the logs of PipelineRun named 'microservice-1' for all Tasks and steps (inc
 	c.Flags().BoolVarP(&opts.Follow, "follow", "f", false, "stream live logs")
 	c.Flags().BoolVarP(&opts.Timestamps, "timestamps", "", false, "show logs with timestamp")
 	c.Flags().BoolVarP(&opts.Prefixing, "prefix", "", true, "prefix each log line with the log source (task name and step name)")
+	c.Flags().BoolVarP(&opts.ExitWithPrError, "exit-with-pipelinerun-error", "E", false, "exit with pipelinerun to the unix shell, 0 if success, 1 if error, 2 if there is an unknown status")
 	c.Flags().StringSliceVarP(&opts.Tasks, "task", "t", []string{}, "show logs for mentioned Tasks only")
 	c.Flags().IntVarP(&opts.Limit, "limit", "", defaultLimit, "lists number of PipelineRuns")
 	return c
@@ -109,7 +113,31 @@ func Run(opts *options.LogOptions) error {
 
 	log.NewWriter(log.LogTypePipeline, opts.Prefixing).Write(opts.Stream, logC, errC)
 
+	// get pipelinerun status
+	if opts.ExitWithPrError {
+		clients, err := opts.Params.Clients()
+		if err != nil {
+			return err
+		}
+		ctx := context.Background()
+		pr, err := clients.Tekton.TektonV1().PipelineRuns(opts.Params.Namespace()).Get(ctx, opts.PipelineRunName, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		os.Exit(prStatusToUnixStatus(pr))
+	}
+
 	return nil
+}
+
+func prStatusToUnixStatus(pr *tektonv1.PipelineRun) int {
+	if len(pr.Status.Conditions) == 0 {
+		return 2
+	}
+	if pr.Status.Conditions[0].Status == corev1.ConditionFalse {
+		return 1
+	}
+	return 0
 }
 
 func askRunName(opts *options.LogOptions) error {
